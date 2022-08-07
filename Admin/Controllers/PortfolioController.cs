99ba179ed73b1826187d13107130e1bc4ed469ea
ahsan7266 <74853314+ahsan7266@users.noreply.github.com>
2022-08-07 14:10:20 -0700

    check

--- Admin/Controllers/PortfolioController.cs
+++ Admin/Controllers/PortfolioController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace Admin.Controllers
 {
@@ -16,7 +19,54 @@ namespace Admin.Controllers
 
         public IActionResult AddPortfolioDetail()
         {
+            if (HttpContext.Session.GetString("Token") == null)
+            {
+                return Redirect("/Account/Login");
+            }
+            ViewBag.Name = HttpContext.Session.GetString("FirstName") + ' ' + HttpContext.Session.GetString("LastName");
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddPortfolioDetail(PersonalInfoViewModel Personalinfo)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                MultipartFormDataContent multiForm = new MultipartFormDataContent();
+                IFormFile file = Personalinfo.Backgroundimg;
+                if (file.Length > 0)
+                {
+                    ByteArrayContent bytes;
+                    byte[] data;
+                    using (var br = new BinaryReader(file.OpenReadStream()))
+                    {
+                       data = br.ReadBytes((int)file.Length);
+                    }
+                    bytes = new ByteArrayContent(data);
+                    //multiForm.Add(bytes, "Backgroundimg",file.FileName); 
+                    Personalinfo.Backgroundimgbyte = bytes;
+                }
+                client.BaseAddress = Constrant.AppUrl;
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                //POST Method
+                HttpResponseMessage response = await client.PostAsJsonAsync("api/Portfolio/AddOrUpdatePersonalInfo", Personalinfo);
+                if (response.IsSuccessStatusCode)
+                {
+                    var Data = await response.Content.ReadAsStringAsync();
+                    var Result = JsonConvert.DeserializeObject<Response<string>>(Data);
+
+                    //return Redirect("/Admin/Dashboard");
+                    return View();
+
+                }
+                else
+                {
+                    var Data = await response.Content.ReadAsStringAsync();
+                    var Result = JsonConvert.DeserializeObject<Response<string>>(Data);
+                    Console.WriteLine("Internal server Error");
+                    return View();
+                }
+            }
         }
     }
 }
---
+++ Admin/Models/PersonalInfoViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Admin.Models
+{
+    public class PersonalInfoViewModel
+    {
+        public Guid PeronalInfoId { get; set; }
+        public IFormFile? Backgroundimg { get; set; }
+        public ByteArrayContent Backgroundimgbyte { get; set; }
+        public IFormFile? Profileimg { get; set; }
+        public IFormFile? Cv { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Country { get; set; }
+        public string? City { get; set; }
+        public int Age { get; set; }
+        public string? Detail { get; set; }
+        public string? Experience { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
