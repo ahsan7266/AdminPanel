9aa5ebd7e362166582d3899234af35f688ca9667
ahsan7266 <74853314+ahsan7266@users.noreply.github.com>
2022-08-14 07:58:31 -0700

    done

--- Admin/Program.cs
+++ Admin/Program.cs
@@ -8,9 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<FormOptions>(options =>
 {
     // Set the limit to 128 MB
-    options.MultipartBodyLengthLimit = 209715200;
-    options.BufferBodyLengthLimit = 209715200;
-    options.ValueLengthLimit = 209715200;
+    options.MultipartBodyLengthLimit = 509715200;
+    options.BufferBodyLengthLimit = 509715200;
+    options.ValueLengthLimit = 509715200;
 });
 
 
@@ -18,27 +18,27 @@ var customBinding = new WSHttpBinding(SecurityMode.Transport, false);
 
 
 customBinding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Certificate;
-customBinding.ReaderQuotas.MaxDepth = 2147483647;
-customBinding.ReaderQuotas.MaxStringContentLength = 2147483647;
-customBinding.ReaderQuotas.MaxArrayLength = 2147483647;
-customBinding.ReaderQuotas.MaxBytesPerRead = 2147483647;
-customBinding.ReaderQuotas.MaxNameTableCharCount = 2147483647;
+customBinding.ReaderQuotas.MaxDepth = 509715200;
+customBinding.ReaderQuotas.MaxStringContentLength = 509715200;
+customBinding.ReaderQuotas.MaxArrayLength = 509715200;
+customBinding.ReaderQuotas.MaxBytesPerRead = 509715200;
+customBinding.ReaderQuotas.MaxNameTableCharCount = 509715200;
 
-customBinding.CloseTimeout = new TimeSpan(0, 10, 0);
-customBinding.OpenTimeout = new TimeSpan(0, 10, 0);
-customBinding.ReceiveTimeout = new TimeSpan(0, 10, 0);
-customBinding.SendTimeout = new TimeSpan(0, 10, 0);
+customBinding.CloseTimeout = new TimeSpan(1, 10, 0);
+customBinding.OpenTimeout = new TimeSpan(1, 10, 0);
+customBinding.ReceiveTimeout = new TimeSpan(1, 10, 0);
+customBinding.SendTimeout = new TimeSpan(1, 10, 0);
 customBinding.BypassProxyOnLocal = false;
 customBinding.TransactionFlow = false;
-customBinding.MaxBufferPoolSize = 2147483647;
-customBinding.MaxReceivedMessageSize = 2147483647;
+customBinding.MaxBufferPoolSize = 509715200;
+customBinding.MaxReceivedMessageSize = 509715200;
 customBinding.TextEncoding = Encoding.UTF8;
 customBinding.UseDefaultWebProxy = true;
 customBinding.AllowCookies = false;
 
 
 customBinding.ReliableSession.Ordered = true;
-customBinding.ReliableSession.InactivityTimeout = new TimeSpan(0, 10, 0);
+customBinding.ReliableSession.InactivityTimeout = new TimeSpan(1, 10, 0);
 
 
 
